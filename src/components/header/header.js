7fe3df94988b153dd58c50e9e0f16f0cbfdc191e
Alex <oleksandr.kurmoiarov@smartexe.com>
2020-01-23 16:45:57 +0200

    Finished sign-in sign-up methods

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,23 +6,26 @@ import { auth } from '../../firebase/firebase.utils'
 
 import './header.style.scss';
 
-const Header = ({currentUser}) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
+const Header = ({currentUser}) => { 
+  console.log(currentUser)
+  return (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
       </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-      {currentUser ? <div className='option'  onClick={() => auth.signOut()}>SIGN OUT</div>
-      :
-      <Link className='option' to='signin'>SIGN IN</Link>}
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        {currentUser ? <div className='option'  onClick={() => auth.signOut()}>SIGN OUT</div>
+        :
+        <Link className='option' to='signin'>SIGN IN</Link>}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Header;   
