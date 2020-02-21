9b34d4913ba082b79977b27d4b06cbe94f302af5
Alex <oleksandr.kurmoiarov@smartexe.com>
2020-02-21 17:37:56 +0200

    Some style changes for mobile responsiveness

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store, persistor }  from './redux/store';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { PersistGate } from 'redux-persist/integration/react';
-import CartProvider from './providers/cart/cart.provider'
+import CartProvider from './providers/cart/cart.provider';
+
+import { GlobalStyle } from './global.styles';
 
 
 ReactDOM.render(
     <Provider store={store}>
         <CartProvider>
             <PersistGate persistor={persistor}>
+                <GlobalStyle/>
                 <App />
             </PersistGate>
         </CartProvider>
