35d9d243f7f12d41536d8debb3ab8b5150e40c61
Alex <oleksandr.kurmoiarov@smartexe.com>
2020-02-25 14:30:25 +0200

    Make app as PWA. Added service worker, manifest.

--- client/src/pages/homepage/HomePage.js
+++ client/src/pages/homepage/HomePage.js
@@ -2,10 +2,22 @@ import React from 'react';
 import Directory from '../../components/directory/Directory';
 
 import { HomePageContainer } from './homepage.styles';
+import { Profiler } from 'react';
 
 const HomePage = () => (
   <HomePageContainer>
-    <Directory />
+    <Profiler 
+      id="Directory"
+      onRender={(id, phase, actualDuration) => {
+        console.log({
+          id,
+          phase,
+          actualDuration
+        })
+      } 
+    }>
+     <Directory />
+    </Profiler>
   </HomePageContainer>
 );
 
